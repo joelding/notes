525b313fed71e0767f100819c49e445f17c8ae84
Joel Ding <joel.ding@avermedia.com>
2015-05-13 14:40:29 +0800

    new file:   getopt.c

---
+++ tmp/getopt.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h> /* for getopt */
+#include <stdlib.h> /* for atoi */
+
+int main(int argc, char *argv[])
+{
+	int opt;
+	int flags = 0;
+	int nsecs = 0;
+
+	while ((opt = getopt(argc, argv, "nt")) != -1) {
+		switch (opt) {
+			case 'n':
+				flags = 1;
+				break;
+
+			case 't':
+//				nsecs = atoi(optarg); // This causes segmentation fault?!?
+				nsecs = atoi(argv[optind]);
+				break;
+
+			case '?':
+			case ':':
+			default:
+				printf("Unknown %c\n", opt);
+				break;
+		}
+	}
+				
+	printf("flags=%d\n", flags);
+	printf("nsecs=%d\n", nsecs);
+
+	return 0;
+}
